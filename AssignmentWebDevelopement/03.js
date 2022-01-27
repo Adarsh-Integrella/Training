0265d460664b113146916a3ca1c34b8ca4ac78a2
Adarsh-Integrella <adarsh.kumar@integrella.com>
2022-01-27 15:12:11 +0530

    deleted files

--- AssignmentWebDevelopement/03.js
+++
@@ -1,8 +0,0 @@
-//3) Write a program that asks the user for a number n and prints the sum of the numbers 1 to n.
-
-let input = prompt("Please enter a number for sum from 1 to n.");
-let temp = 0;
-for (let i = 1; i <= input; i++) {
-  temp += i;
-}
-console.log(temp);
--- AssignmentWebDevelopement/08.js
+++
@@ -1,8 +0,0 @@
-//8) Write a function that returns the largest element in a list.
-
-const list = [45, 56, 3, 34, 109, 343, 234, 44, 789, 32, 89];
-let maxNum = Number.NEGATIVE_INFINITY;
-for (let x of list) {
-  maxNum = Math.max(x, maxNum);
-}
-console.log(maxNum);
